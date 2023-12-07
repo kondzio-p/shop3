f8c78981b04e595da09a014cd4396d69f6b8a99d
unknown <konradczater@gmail.com>
2023-12-07 10:21:23 +0100

    wstrzykiwanie stron do main.html

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -7,8 +7,8 @@ def store(request):
 
 def cart(request):
     context = {}
-    return render(request, 'store/store.html', context)
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
     context = {}
-    return render(request, 'store/store.html', context)
+    return render(request, 'store/checkout.html', context)
